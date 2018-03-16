cfefbfd3baf787c4bbe33e504f8ad90a08a55fa7
Aaron Jensen <aaronjensen@gmail.com>
2018-03-16 10:04:38 -0700

    Remove deprecated javascript and stylesheet registration

--- lib/active_admin/editor/engine.rb
+++ lib/active_admin/editor/engine.rb
@@ -6,22 +6,6 @@ module ActiveAdmin
       ActiveAdmin.application.class_eval do
         def editor; ActiveAdmin::Editor.configuration end
       end
-
-      initializer 'active_admin.editor', :group => :all do |app|
-        app.config.assets.precompile += [
-          'active_admin/editor.js',
-          'active_admin/editor.css'
-        ] + ActiveAdmin::Editor.configuration.stylesheets
-
-        ActiveAdmin.application.tap do |config|
-          config.register_javascript 'active_admin/editor.js'
-          config.register_stylesheet 'active_admin/editor.css'
-
-          ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
-            config.register_stylesheet stylesheet
-          end
-        end
-      end
     end
   end
 end
